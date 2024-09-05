98470034930d41f31650a09bd85abe405cc81992
abdelrahmanabdelsamie7 <abdelrhmanabdelsamie@gmail.com>
2024-09-05 18:50:15 +0300

    Finsih Confirm Message

--- src/app/Register/confirm-message/confirm-message.component.ts
+++ src/app/Register/confirm-message/confirm-message.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-confirm-message',
   templateUrl: './confirm-message.component.html',
-  styleUrls: ['./confirm-message.component.css']
+  styleUrls: ['./confirm-message.component.css'],
 })
 export class ConfirmMessageComponent {
-
+  isSucess = false;
 }
